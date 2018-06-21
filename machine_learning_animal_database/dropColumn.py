50f574e73479737a3fd6792326bf4cc96cc38515
tatiana <tatiana.sca@gmail.com>
2018-06-21 19:38:05 -0300

    cleaning files to keep important data only

--- machine_learning_animal_database/dropColumn.py
+++ machine_learning_animal_database/dropColumn.py
@@ -6,8 +6,8 @@ import string as st
 #df.drop(df.index[2]) # by index 
 #df.drop(df.index[[2,3]])
 
-file_path = 'species.csv'
-filtered_path = 'species_filtered.csv'
+file_path = 'test.csv'
+filtered_path = 'test_filtered.csv'
 
 with open(file_path, 'r') as file:
     lines = file.readlines()
@@ -27,7 +27,7 @@ results = pd.read_csv(filtered_path, low_memory = False)
 header_list = list(results)
 print('Header list length {}'.format(len(header_list)))
 
-keep_list = ['species_number', 'common_name', 'kingdom', 'family']
+keep_list = ['test_id','species_number']
 
 # chemical_carrier keep_list = ['carrier_id', 'cas_number', 'test_id', 'chem_name']
 
@@ -39,12 +39,12 @@ keep_list = ['species_number', 'common_name', 'kingdom', 'family']
 
 # test keep_list = ['test_id', 'reference_number', 'test_characteristics', 'species_number', 'test_characteristics', 'test_formulation']
 
-#spicies ['species_number', 'common_name', 'kingdom', 'family']
+#species ['species_number', 'common_name', 'kingdom', 'family']
 
 drop_list = [identifier for identifier in header_list if identifier not in keep_list]
 
 results = results.drop(drop_list, axis=1)
 
-results.to_csv("final_species.csv", sep=',')
+results.to_csv("clean_test.csv", sep=',')
 
 #print(results)
